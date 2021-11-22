872227a322459cb3677f3027f8d02a153cf5cd83
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2021-11-22 14:44:36 +0530

    💄 feat: add interpolate background color

--- PhilzCoffee/src/App.js
+++ PhilzCoffee/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import {Dimensions, View, ScrollView, StyleSheet} from 'react-native';
-
+import Animated, {
+  interpolateColor,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+//import {mixColor} from 'react-native-redash';
 import {products} from './Model';
 import Card, {CARD_HEIGHT} from './Card';
 import Products from './Products';
@@ -12,21 +18,48 @@ const styles = StyleSheet.create({
   slider: {height: CARD_HEIGHT},
 });
 
+const snapToOffsets = [0, CARD_HEIGHT];
+
 const PhilzCoffee = () => {
+  const translateX = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: ({contentOffset: {x}}) => {
+      translateX.value = x;
+    },
+  });
+  const style = useAnimatedStyle(() => ({
+    flex: 1,
+    backgroundColor: interpolateColor(
+      translateX.value,
+      products.map((_, i) => width * i),
+      products.map(product => product.color2),
+    ),
+  }));
   return (
-    <View style={{flex: 1}}>
-      <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
+    <Animated.View style={style}>
+      <ScrollView
+        snapToOffsets={snapToOffsets}
+        decelerationRate="fast"
+        snapToEnd={false}
+        bounces={false}
+        showsVerticalScrollIndicator={false}>
         <View style={styles.slider}>
-          <ScrollView decelerationRate={false} snapToInterval={width} showsHorizontalScrollIndicator={false} horizontal>
+          <Animated.ScrollView
+            scrollEventThrottle={16}
+            onScroll={onScroll}
+            decelerationRate="fast"
+            snapToInterval={width}
+            showsHorizontalScrollIndicator={false}
+            horizontal>
             {products.map((product, index) => (
               <Card product={product} key={index} />
             ))}
-          </ScrollView>
+          </Animated.ScrollView>
           <Products />
         </View>
         <Cards />
       </ScrollView>
-    </View>
+    </Animated.View>
   );
 };
 
