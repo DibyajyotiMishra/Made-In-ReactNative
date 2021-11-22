339c8581a1632c6234a2902789f46a787fb8a9e0
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2021-11-22 16:45:27 +0530

    🎨 chore: initial Setups

--- DialLogin/index.js
+++ DialLogin/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {RotatoryLogin as App} from './src/RotatoryLogin';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
