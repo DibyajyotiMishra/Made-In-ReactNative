5516cd4808c384f2e29d4198182beccd2be9e1a9
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2021-11-25 00:12:14 +0530

    ✨ feat: add Card Animation

--- SwipeGesture/src/Card.js
+++ SwipeGesture/src/Card.js
@@ -4,37 +4,107 @@ import {PanGestureHandler} from 'react-native-gesture-handler';
 import Animated, {
   Easing,
   useAnimatedGestureHandler,
-  useAnimatedReaction,
   useAnimatedStyle,
   useSharedValue,
-  withDelay,
   withSpring,
   withTiming,
+  withDelay,
+  useAnimatedReaction,
 } from 'react-native-reanimated';
 import {snapPoint} from 'react-native-redash';
 
-const {width: wWidth, height} = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
-const SNAP_POINTS = [-wWidth, 0, wWidth];
 const aspectRatio = 722 / 368;
-const CARD_WIDTH = wWidth - 128;
+const CARD_WIDTH = width - 128;
 const CARD_HEIGHT = CARD_WIDTH * aspectRatio;
 const IMAGE_WIDTH = CARD_WIDTH * 0.9;
 const DURATION = 250;
+const side = (width + CARD_WIDTH + 50) / 2;
+const SNAP_POINTS = [-side, 0, side];
 
 const Card = ({card: {source}, shuffleBack, index}) => {
+  const x = useSharedValue(0);
+  const y = useSharedValue(-height);
+  const theta = Math.random() * 20 - 10;
+  const rotateZ = useSharedValue(0);
+  const scale = useSharedValue(1);
+  useAnimatedReaction(
+    () => shuffleBack.value,
+    () => {
+      if (shuffleBack.value) {
+        const delay = 150 * index;
+        x.value = withDelay(delay, withSpring(0));
+        rotateZ.value = withDelay(delay, withSpring(theta), {}, () => {
+          shuffleBack.value = false;
+        });
+      }
+    },
+  );
+  useEffect(() => {
+    const delay = 1000 + index * DURATION;
+    y.value = withDelay(
+      delay,
+      withTiming(0, {
+        duration: DURATION,
+        easing: Easing.inOut(Easing.ease),
+      }),
+    );
+    rotateZ.value = withDelay(
+      delay,
+      withTiming(theta, {
+        duration: DURATION,
+        easing: Easing.inOut(Easing.ease),
+      }),
+    );
+  }, [index, y]);
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.x = x.value;
+      ctx.y = y.value;
+      scale.value = withTiming(1.1, {easing: Easing.inOut(Easing.ease)});
+      rotateZ.value = withTiming(0, {easing: Easing.inOut(Easing.ease)});
+    },
+    onActive: ({translationX, translationY}, ctx) => {
+      x.value = ctx.x + translationX;
+      y.value = ctx.y + translationY;
+    },
+    onEnd: ({velocityX, velocityY}) => {
+      const dest = snapPoint(x.value, velocityX, SNAP_POINTS);
+      x.value = withSpring(dest, {velocity: velocityX});
+      y.value = withSpring(0, {velocity: velocityY});
+      scale.value = withTiming(1, {easing: Easing.inOut(Easing.ease)}, () => {
+        if (index === 0 && dest !== 0) {
+          shuffleBack.value = true;
+        }
+      });
+    },
+  });
+
+  const style = useAnimatedStyle(() => ({
+    transform: [
+      {perspective: 1500},
+      {rotateZ: `${rotateZ.value}deg`},
+      {rotateX: '30deg'},
+      {translateX: x.value},
+      {translateY: y.value},
+      {scale: scale.value},
+    ],
+  }));
   return (
     <View style={styles.container} pointerEvents="box-none">
-      <View style={[styles.card]}>
-        <Image
-          source={source}
-          style={{
-            width: IMAGE_WIDTH,
-            height: IMAGE_WIDTH * aspectRatio,
-          }}
-          resizeMode="contain"
-        />
-      </View>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={[styles.card, style]}>
+          <Image
+            source={source}
+            style={{
+              width: IMAGE_WIDTH,
+              height: IMAGE_WIDTH * aspectRatio,
+            }}
+            resizeMode="contain"
+          />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
